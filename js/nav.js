7422d298da1e2295e6b0c00ec2bad08d661dd4d5
cesar pablo <cesarpgasuz@gmail.com>
2021-12-02 22:55:01 -0600

    responsive
    se agrego responsive design

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+
+		  $('#menu-movil').on('click', function(){
+			$('.menu-links').toggleClass('visible');
+			
+		})
+
+		  
+		  
+		/*  
+		$('.sombra').on('click', function(){
+			$('.navegacion .menu-nav').removeClass('visibles');
+			$('body').removeClass('scroll');
+			$('.sombra').removeClass('visible-som');
+		})
+*/
+		$('#closeH').on('click', function(){
+			$('.menu-links').removeClass('visible');
+			/*$('body').removeClass('scroll');
+			$('.sombra').removeClass('visible-som');*/
+		})
+		  
