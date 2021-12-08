c26d7bf5d4739fd668499a89fceb1a5f2511eadb
cesar pablo <cesarpgasuz@gmail.com>
2021-12-08 00:07:33 -0600

    page dependencias
    se crearon las pagina para cada dependencia, tambien se agrego informacion de contacto como telefono, correo y horario, todavia falta recopilar mas informacion del apartado contacto

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,8 @@
 
 		  $('#menu-movil').on('click', function(){
 			$('.menu-links').toggleClass('visible');
+			$('#closeH').css('display', 'block');
+			$('#menu-movil').css('opacity', '0');
 			
 		})
 
@@ -15,7 +17,38 @@
 */
 		$('#closeH').on('click', function(){
 			$('.menu-links').removeClass('visible');
+			$('#closeH').css('display', 'none');
+			$('#menu-movil').css('opacity', '1');
+
 			/*$('body').removeClass('scroll');
 			$('.sombra').removeClass('visible-som');*/
 		})
 		  
+
+		$('#openHorarioEscritorio').on('click', function(){
+			$('.horario').toggleClass('open-horario_escritorio');
+			
+		})
+
+		$('#closeHorarioEscritorio').on('click', function(){
+			$('.horario').removeClass('open-horario_escritorio');
+			
+		})
+
+
+		$('#openHorarioMovil').on('click', function(){
+			$('.horario').toggleClass('open-horario_movil');
+			
+		})
+
+		$('#closeHorarioMovil').on('click', function(){
+			$('.horario').removeClass('open-horario_movil');
+			
+		})
+
+
+
+
+
+
+
